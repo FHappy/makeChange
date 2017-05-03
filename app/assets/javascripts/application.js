dbf21798a77d9abb4c779ba10184651c3e6bdfae
FHappy <Hassan.Masroor@me.com>
2017-05-03 18:49:51 -0400

    initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require materialize-sprockets
 //= require angular/angular
 //= require angular-ui-router/release/angular-ui-router
 //= require materialize
+//= require materialize/extras/nouislider
 //= require_self
 //= require_tree .
 
