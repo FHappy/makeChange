af41fab484ce107944527da8ba7332ce748e7d65
FHappy <Hassan.Masroor@me.com>
2017-05-03 23:02:37 -0400

    meh

--- app/assets/javascripts/components/stripeForm/stripeForm.controller.js
+++ app/assets/javascripts/components/stripeForm/stripeForm.controller.js
@@ -11,16 +11,17 @@ function StripeFormController(StripeCheckout) {
     token: function(token, args) {
       console.log(token.id);
     }
-  })
+  });
 
   function doCheckout(token, args) {
     var options = {
-      description: 'Ten dollahs!',
-      amount: 1000
+      description: 'one hundred dollahs!',
+      amount: 10000
     };
 
     handler.open(options)
       .then(function resolve(response) {
+        console.log(response);
         alert("Got stripe token: " + response[0].id);
       }, function reject(response) {
         alert("Stripe Checkout closed without making a sale: :(");
