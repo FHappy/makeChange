5f4734c7fccc33f2f69e8dfbdd7a2d7fcb4b855e
LiamTRobinson <Liamtrobinson@gmail.com>
2017-05-04 18:35:40 -0400

    get user's charities to show

--- app/assets/javascripts/components/homePage/homePage.controller.js
+++ app/assets/javascripts/components/homePage/homePage.controller.js
@@ -4,12 +4,15 @@ function HomePageController(UsersService) {
   const vm = this;
 
   vm.user = null;
+  vm.charities = null;
 
   function currentUser() {
   	UsersService
   		.getCurrentUser()
   		.then(function(res) {
-  			vm.user = res.data;
+  			vm.user = res.data.user;
+        vm.charities = res.data.charities;
+        console.log(vm.charities);
   		});
   }
 
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,8 @@
 class Api::UsersController < ApplicationController
 	def current
 		@user = current_user
+		@user_charities = @user.charities.uniq
 
-		render json: @user
+		render json: { user: @user, charities: @user_charities }
 	end
 end
