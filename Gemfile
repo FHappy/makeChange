83346876b5e79056a5c139d1d3edf8e0a2e45a74
LiamTRobinson <Liamtrobinson@gmail.com>
2017-05-05 09:36:44 -0400

    before running change column migration

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'omniauth-facebook'
 gem "materialize-sass"
 gem "material_icons"
 gem 'stripe'
+gem "httparty"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/api/charities_controller.rb
+++ app/controllers/api/charities_controller.rb
@@ -1,7 +1,12 @@
 class Api::CharitiesController < ApplicationController
 	def index
-		@charities = Charity.all
+		@charities_local = Charity.all
 
-		render json: @charities
+		render json: @charities_local
+	end
+
+	def show
+		@charity = Charity.find_by ein: params[:ein]
+		render json: @charity
 	end
 end
