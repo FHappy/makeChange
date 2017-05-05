0fe34d771828a170fceeb21a4aab39e094b76ba6
FHappy <Hassan.Masroor@me.com>
2017-05-05 12:16:57 -0400

    de duped charity function working

--- app/controllers/api/charities_controller.rb
+++ app/controllers/api/charities_controller.rb
@@ -2,12 +2,26 @@ class Api::CharitiesController < ApplicationController
 	def index
 		@charities_local = Charity.all
 		@charities_third_party = HTTParty.get("http://data.orghunter.com/v1/charitysearch?user_key=fd8d0a7418b42223ada1b88c40dfa0a9&eligible=1").as_json["data"]
-
-		render json: { local: @charities_local, third_party: @charities_third_party }
+		render json: {charities: de_dupe(@charities_third_party)}
 	end
 
 	def show
 		@charity = Charity.find_by ein: params[:ein]
 		render json: @charity
 	end
+
+	private
+	def de_dupe(charities)
+		@db_charities = Charity.all
+		charities.map! do |org_charity|
+			@db_charities.each do |db_charity|
+				if db_charity["ein"] == org_charity["ein"]
+					org_charity = db_charity
+				end
+			end
+			org_charity
+		end
+		charities
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   
   get "api/users/current" => "api/users#current", as: "users_current"
-  get "api/charities" => "api/charities#index", as: "charities_index"
-  get "api/charities/:ein" => "api/charities#show", as: "charities_show"
+  get "api/charities/index/:page" => "api/charities#index", as: "charities_index"
 
   namespace :api do
     resources :charges, only: [:index, :create]
