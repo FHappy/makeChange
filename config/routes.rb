15ea5d31ec65691b3e53c51df17183a489bb5080
Hassan Masroor <Hassan.Masroor@me.com>
2017-05-06 19:35:33 -0400

    added precompiled assets for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "api/charities" => "api/charities#index", as: "charities_index"
   get "api/charities/:ein" => "api/charities#show", as: "charities_show"
   get "api/charities/search/:query" => "api/charities#search", as: "charities_search"
+  get "api/charities/donate" => "api/charties#donate", as: "charities_donate"
   
   namespace :api do
     resources :charges, only: [:index, :create]
