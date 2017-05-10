23be242a8d008c4b6b2b821231ed49d745be4742
FHappy <Hassan.Masroor@me.com>
2017-05-10 15:41:05 -0400

    quick syntax fix

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ class OmniauthCallbacksController < ApplicationController
   skip_before_action :authenticate_user!
 
   def stripe_connect
-    binding.pry
     @user = User.new(
       username: 'CraigAnderson',
       email: 'canderson@makemydonation.org',
@@ -14,19 +13,11 @@ class OmniauthCallbacksController < ApplicationController
       access_code: request.env["omniauth.auth"].credentials.token,
       publishable_key: request.env["omniauth.auth"].info.stripe_publishable_key
     })
-      # anything else you need to do in response..
       sign_in_and_redirect @user, 'root_path'
     else
       session["devise.stripe_connect_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
-
-  def failure
-    render json: {
-      'oops': "I'm sorry..."
-    }
-  end
   
-
 end
