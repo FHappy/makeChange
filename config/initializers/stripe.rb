af8aee2409956cc981110a1c9f6ed29ffca54010
FHappy <Hassan.Masroor@me.com>
2017-05-10 12:21:45 -0400

    minor syntax fixes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,4 @@
 Rails.configuration.stripe = {
-  # :publishable_key => ENV['pk_test_BXvvyNNCdq2c7PKLgIoHDvnw'],
-  # :secret_key => ENV['sk_test_VMkXZrajoyfxdGbjLZ8l329p']
   :publishable_key => ENV["stripe_publishable_key"],
   :secret_key => ENV["stripe_api_key"]
 }
